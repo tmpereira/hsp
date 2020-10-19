f86b569b61ad8fbb0ef40a2d277f8aece2c6b2b2
tmpereira <54153309+tmpereira@users.noreply.github.com>
2020-10-19 15:48:57 -0300

    Add files via upload

--- km.py
+++ km.py
@@ -2,9 +2,9 @@
 submodulo para clustrerização das imagens usando kmeans 
 '''
 
-def fit(data,k,fold):
+def fit(data,k,fold = 10):
     from sklearn.cluster import KMeans
-    import timeit  
+    import time 
     import numpy as np
     label = np.ones((data['r'].shape[0],k-1))
     centroid = np.ones((1,data['r'].shape[1]))
@@ -12,13 +12,14 @@ def fit(data,k,fold):
     X = data['r']
     print(str(X.shape[0]) + " spectra")
     for i in range(2,k+1):
-            t = timeit.default_timer()
+            t1 = time.time()
             kmeans = KMeans(n_clusters=i,n_init=fold,random_state=0).fit(X)
             label[:,i-2] = (kmeans.labels_+1)
+            t2 = time.time()
             centroid = np.vstack((centroid,kmeans.cluster_centers_))
             k_centroid = np.vstack((k_centroid,np.tile(i,(i,1))))
-            print( str(i) + " cluster")
-            print(str(np.round(timeit.default_timer() - t,decimals=2)) + " seconds")
+            print( i,' clusters   - ',t2-t1,' seconds' )
+            
     data['km_label'] = label 
     data['km_centroid']=centroid
     data['km_k_centroid'] = k_centroid
@@ -49,7 +50,50 @@ def sh(data,k):
     plt.axes([0.85, 0.1, 0.05, 0.75])
     plt.pcolor(np.arange(14)[:,None],cmap=cmap, norm=norm)
     
-
+def shfv(data,k):
+    import numpy as np
+    import matplotlib.pyplot as plt 
+    import matplotlib.colors as colors
+    dplot = np.zeros((data['dx']*data['dy']))
+    dplot[data['sel']] =data['km_label'][:,k-2]
+    dplot =dplot.reshape(data['dx'],data['dy'])
+    dplot = dplot[::-1,:]
+    n = np.max([data['dx'],data['dy']])
+    colmap = [ (0,0,0), (1,0,0),(0,1,0),(0,0,1),(0.41,0.41,0.41),(0,1,1),
+    (0.58,0,0.82),(0,0.50,0),(0.98,0.50,0.44),(1,	1,0.87),
+    (0.39,0.58,0.92),(0.50,0.50,0),(1,0.89,0.76),(0.96,0.96,0.86),
+    (0,1,1)]        
+    cmap = colors.ListedColormap(colmap)
+    boundaries = list(range(15))
+    norm = colors.BoundaryNorm(boundaries, cmap.N, clip=True)  
+    plt.axes([0.1, 0.1, (data['dy']/n)*0.7,(data['dx']/n)*0.7])
+    plt.pcolor(dplot,cmap=cmap, norm=norm)
+    plt.axes([0.85, 0.1, 0.05, 0.75])
+    plt.pcolor(np.arange(14)[:,None],cmap=cmap, norm=norm)
+    
+    
+def shfh(data,k):
+    import numpy as np
+    import matplotlib.pyplot as plt 
+    import matplotlib.colors as colors
+    dplot = np.zeros((data['dx']*data['dy']))
+    dplot[data['sel']] =data['km_label'][:,k-2]
+    dplot =dplot.reshape(data['dx'],data['dy'])
+    dplot = dplot[:,::-1]
+    n = np.max([data['dx'],data['dy']])
+    colmap = [ (0,0,0), (1,0,0),(0,1,0),(0,0,1),(0.41,0.41,0.41),(0,1,1),
+    (0.58,0,0.82),(0,0.50,0),(0.98,0.50,0.44),(1,	1,0.87),
+    (0.39,0.58,0.92),(0.50,0.50,0),(1,0.89,0.76),(0.96,0.96,0.86),
+    (0,1,1)]        
+    cmap = colors.ListedColormap(colmap)
+    boundaries = list(range(15))
+    norm = colors.BoundaryNorm(boundaries, cmap.N, clip=True)  
+    plt.axes([0.1, 0.1, (data['dy']/n)*0.7,(data['dx']/n)*0.7])
+    plt.pcolor(dplot,cmap=cmap, norm=norm)
+    plt.axes([0.85, 0.1, 0.05, 0.75])
+    plt.pcolor(np.arange(14)[:,None],cmap=cmap, norm=norm)
+    
+    
 def spc(data,k):
     import numpy as np
     import matplotlib.pyplot as plt 
@@ -61,3 +105,4 @@ def spc(data,k):
         sel = data['km_label'][:,k-2] == i+1
         plt.plot(data['wn'],np.mean(data['r'][sel,:],axis=0),color=cmap[i]
         , linewidth=2)
+
